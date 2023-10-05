73474b281c43f92376a93cc88b28d82e6286d516
Vinicius Batista <mrbrazdev@gmail.com>
2023-10-05 14:01:30 -0300

    Implementing endpoint to create a new user

--- api/src/main/java/com/backend/api/controller/UserController.java
+++ api/src/main/java/com/backend/api/controller/UserController.java
@@ -1,7 +1,12 @@
 package com.backend.api.controller;
 
+import com.backend.api.entity.User;
 import com.backend.api.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -13,4 +18,7 @@ public class UserController {
     public void setUserService(UserService userService){
         this.userService = userService;
     }
+
+    @PostMapping(value = "/api/users")
+
 }
