4f4b57473cc3b6b1b8ec2b9bc4efac1e6fe6e990
Vinicius Batista <mrbrazdev@gmail.com>
2023-10-05 14:05:28 -0300

    Implementing a endpoint to list all users

--- api/src/main/java/com/backend/api/controller/TaskController.java
+++ api/src/main/java/com/backend/api/controller/TaskController.java
@@ -2,6 +2,7 @@ package com.backend.api.controller;
 
 import com.backend.api.service.TaskService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,6 +17,8 @@ public class TaskController {
     }
 
     @PostMapping(value = "/api/tasks")
+
+    @GetMapping(value = "/api/users")
 
 
 }
