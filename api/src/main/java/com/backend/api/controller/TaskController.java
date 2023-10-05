b3e343ffb4004ef23e497c374e513f6f511d4d47
Vinicius Batista <mrbrazdev@gmail.com>
2023-10-05 13:56:43 -0300

    Addeed dependency injection for TaskService

--- api/src/main/java/com/backend/api/controller/TaskController.java
+++ api/src/main/java/com/backend/api/controller/TaskController.java
@@ -1,4 +1,18 @@
 package com.backend.api.controller;
 
+import com.backend.api.service.TaskService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class TaskController {
+
+    private TaskService taskService;
+
+    @Autowired
+    public void setTaskService (TaskService taskService){
+        this.taskService = taskService;
+    }
+
+
 }
