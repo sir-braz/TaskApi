afdb9a18b2a9eb85280b15d0d174f7f160af6243
Vinicius Batista <mrbrazdev@gmail.com>
2023-10-05 14:06:34 -0300

    Implementing a endpoint to edit users with id

--- api/src/main/java/com/backend/api/controller/TaskController.java
+++ api/src/main/java/com/backend/api/controller/TaskController.java
@@ -4,6 +4,7 @@ import com.backend.api.service.TaskService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -21,6 +22,8 @@ public class TaskController {
     @GetMapping(value = "/api/users")
 
     @GetMapping(value = "/api/users/{id}")
+
+    @PutMapping(value = "/api/users/{id}")
 
 
 }
