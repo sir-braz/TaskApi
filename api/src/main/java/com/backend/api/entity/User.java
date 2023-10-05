9b09cb72905e7d486d6e3aac5fcffd11f1b48d90
Vinicius Batista <mrbrazdev@gmail.com>
2023-10-05 13:50:20 -0300

    Implementing core entity users

--- api/src/main/java/com/backend/api/entity/User.java
+++ api/src/main/java/com/backend/api/entity/User.java
@@ -1,4 +1,18 @@
 package com.backend.api.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String username;
+
+    private String email;
 }
