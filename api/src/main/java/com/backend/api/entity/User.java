bec74151c42d7ba9f5d7f23e25e1fed6c90b5abf
Vinicius Batista <mrbrazdev@gmail.com>
2023-10-05 13:50:44 -0300

    Implementing core entity users

--- api/src/main/java/com/backend/api/entity/User.java
+++ api/src/main/java/com/backend/api/entity/User.java
@@ -15,4 +15,28 @@ public class User {
     private String username;
 
     private String email;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
