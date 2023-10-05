b40c65946e778d612fc8f367b3b2ea406365c262
Vinicius Batista <mrbrazdev@gmail.com>
2023-10-05 13:57:50 -0300

    Addeed dependency injection for UserService

--- api/src/main/java/com/backend/api/controller/UserController.java
+++ api/src/main/java/com/backend/api/controller/UserController.java
@@ -1,4 +1,16 @@
 package com.backend.api.controller;
 
+import com.backend.api.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class UserController {
+
+    private UserService userService;
+
+    @Autowired
+    public void setUserService(UserService userService){
+        this.userService = userService;
+    }
 }
