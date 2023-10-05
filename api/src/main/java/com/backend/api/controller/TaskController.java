cb2f1a778158bee83c5cc7290c5fa0095dfad7a5
Vinicius Batista <mrbrazdev@gmail.com>
2023-10-05 14:08:40 -0300

    Implementing a endpoint to delete tasks with id

--- api/src/main/java/com/backend/api/controller/TaskController.java
+++ api/src/main/java/com/backend/api/controller/TaskController.java
@@ -2,10 +2,7 @@ package com.backend.api.controller;
 
 import com.backend.api.service.TaskService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class TaskController {
@@ -24,6 +21,8 @@ public class TaskController {
     @GetMapping(value = "/api/tasks/{id}")
 
     @PutMapping(value = "/api/tasks/{id}")
+
+    @DeleteMapping(value = "/api/tasks/{id}")
 
 
 }
