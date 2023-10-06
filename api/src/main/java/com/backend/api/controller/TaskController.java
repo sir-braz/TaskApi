61d5b14e4e6f12275bfe43ae31a1a211ad0efefc
Vinicius Batista <mrbrazdev@gmail.com>
2023-10-06 11:41:26 -0300

    Implementing a error and exception handling in task controller

--- api/src/main/java/com/backend/api/controller/TaskController.java
+++ api/src/main/java/com/backend/api/controller/TaskController.java
@@ -2,9 +2,12 @@ package com.backend.api.controller;
 
 import com.backend.api.entity.Task;
 import com.backend.api.service.TaskService;
+import com.backend.api.utility.BackendException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 
@@ -20,27 +23,47 @@ public class TaskController {
 
     @PostMapping(value = "/api/tasks")
     public ResponseEntity<Task> createTask(){
-
+        try{
+            return null;
+        }catch (BackendException ex){
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null);
+        }
     }
 
     @GetMapping(value = "/api/tasks")
     public ResponseEntity<List<Task>> listAllTask(@RequestBody Task task){
-
+        try{
+            return null;
+        }catch (BackendException ex){
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null);
+        }
     }
 
     @GetMapping(value = "/api/tasks/{id}")
     public ResponseEntity<List<Task>> listTask(@PathVariable Long taskId){
-
+        try{
+            return null;
+        }catch (BackendException ex){
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null);
+        }
     }
 
     @PutMapping(value = "/api/tasks/{id}")
     public ResponseEntity<Task> updateTask(@PathVariable Long id, @RequestBody Task task){
-
+        try{
+            return null;
+        }catch (BackendException ex){
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null);
+        }
     }
 
     @DeleteMapping(value = "/api/tasks/{id}")
     public ResponseEntity<Task> deleteTask(@PathVariable Long taskId){
-
+        try{
+            return null;
+        }catch (BackendException ex){
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null);
+        }
     }
 
 
