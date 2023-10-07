7b60a1079e4482c013b6f15dd05fcf53279b5bb3
Vinicius Batista <mrbrazdev@gmail.com>
2023-10-07 10:23:03 -0300

    Implementing error and exception handling

--- api/src/main/java/com/backend/api/controller/UserController.java
+++ api/src/main/java/com/backend/api/controller/UserController.java
@@ -2,9 +2,12 @@ package com.backend.api.controller;
 
 import com.backend.api.entity.User;
 import com.backend.api.service.UserService;
+import com.backend.api.utility.BackendException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 
@@ -20,27 +23,47 @@ public class UserController {
 
     @PostMapping(value = "/api/users")
     public ResponseEntity<User> createUser(@RequestBody User user){
-
+        try{
+            return null;
+        }catch (BackendException ex){
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null);
+        }
     }
 
     @GetMapping(value = "/api/users")
     public ResponseEntity<List<User>> listAllUsers(){
-
+        try{
+            return null;
+        }catch (BackendException ex){
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null);
+        }
     }
 
     @GetMapping(value = "/api/users/{id}")
     public ResponseEntity<List<User>> listUser(@PathVariable long userId){
-
+        try{
+            return null;
+        }catch (BackendException ex){
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null);
+        }
     }
 
     @PutMapping(value = "/api/users/{id}")
     public ResponseEntity<User> updateUser(){
-
+        try{
+            return null;
+        }catch (BackendException ex){
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null);
+        }
     }
 
     @DeleteMapping(value = "/api/users/{id}")
     public ResponseEntity<User> deleteUser(){
-
+        try{
+            return null;
+        }catch (BackendException ex){
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null);
+        }
     }
 
 }
