a626d6e4a88b3ea6c682c2a8e0f0ea6e7390cb39
Vinicius Batista <mrbrazdev@gmail.com>
2023-10-08 19:04:02 -0300

    Creating the UserService interface with methods for managing users

--- api/src/main/java/com/backend/api/service/UserService.java
+++ api/src/main/java/com/backend/api/service/UserService.java
@@ -1,4 +1,15 @@
 package com.backend.api.service;
 
+import com.backend.api.entity.User;
+
+import java.util.List;
+
 public interface UserService {
+    User createUser(User user);
+
+    List<User> getAllUsers();
+
+    User getUserById(long id);
+
+    void deleteUserById(Long id);
 }
