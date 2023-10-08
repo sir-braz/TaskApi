6f156f7ce482a59b84f3f20222d86c24a95f7572
Vinicius Batista <mrbrazdev@gmail.com>
2023-10-08 14:59:09 -0300

    Implementing the TaskServiceImpl class with CRUD methods for managing tasks

--- api/src/main/java/com/backend/api/service/TaskServiceImpl.java
+++ api/src/main/java/com/backend/api/service/TaskServiceImpl.java
@@ -1,4 +1,33 @@
 package com.backend.api.service;
 
-public class TaskServiceImpl {
+import com.backend.api.entity.Task;
+
+import java.util.List;
+
+public class TaskServiceImpl implements TaskService{
+
+    @Override
+    public Task createTask(Task task) {
+        return null;
+    }
+
+    @Override
+    public List<Task> getAllTasks() {
+        return null;
+    }
+
+    @Override
+    public Task getTaskById(Long id) {
+        return null;
+    }
+
+    @Override
+    public Task updateTask(Task existingTask) {
+        return null;
+    }
+
+    @Override
+    public boolean deleteTaskById(Long id) {
+        return false;
+    }
 }
